2d1d8e1adb33b84e75e9a5ed9e611a83f8f77552
shahadh <shahadh.hamza@gmail.com>
2021-09-01 01:42:08 +0530

    asd

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['https://safe-gorge-82079.herokuapp.com'],
 
     'allowed_origins_patterns' => [],
 
