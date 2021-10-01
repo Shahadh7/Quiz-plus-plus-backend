687a6182e5138ae2ac1a2296ab11d7e32aebbd56
shahadh <shahadh.hamza@gmail.com>
2021-10-02 00:02:33 +0530

    changes

--- app/Http/Controllers/ExamController.php
+++ app/Http/Controllers/ExamController.php
@@ -7,6 +7,7 @@ use App\Models\Exam;
 use App\Models\Quiz;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Str;
 
 class ExamController extends Controller
